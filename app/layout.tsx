d97dd18326a86ee1f4b964e03013137fed4fa96c
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-10 21:37:18 +0100

    style: Definuj styl pro body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css';
 
+import clsx from 'clsx';
 import { ReactNode } from 'react';
+
+import { inter } from '@/styles/fonts';
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -9,7 +12,15 @@ interface RootLayoutProps {
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang='cs'>
-      <body className='antialiased'>{children}</body>
+      <body
+        className={clsx(
+          inter.className,
+          'flex min-h-screen flex-col overflow-x-hidden bg-bg-body',
+          'text-text-primary antialiased'
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 };
