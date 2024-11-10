01d8375241344d9c801b0841ce7cea0f509272c4
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-10 22:08:33 +0100

    style: Přidej vlastní breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,10 +25,18 @@ export default {
       backdropBlur: {
         tiny: '2px',
       },
+      screens: {
+        '2xl': '1280px',
+      },
       container: {
         center: true,
         padding: {
-          DEFAULT: '0.5rem',
+          DEFAULT: '1rem',
+          sm: '1.5rem',
+          md: '2rem',
+          lg: '2.5rem',
+          xl: '3rem',
+          '2xl': '3.5rem',
         },
       },
     },
