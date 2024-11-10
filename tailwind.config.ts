0b1d225db58c64eb6620ed294476311cae4589bb
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-10 21:29:21 +0100

    style: Vytvoř základní definice v tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,32 @@ export default {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-body': '#141414',
+        'bg-primary': '#1c1c1cf2',
+        'bg-secondary': '#262626f2',
+        'text-primary': '#cccccc',
+        'text-secondary': '#aaaaaa',
+        'brand-primary': '#00cc33',
+        'brand-secondary': '#663399',
+        'accent-primary': '#ff4d4d',
+        'border-primary': '#4a4a4af2',
+      },
+      boxShadow: {
+        'brand-primary': '0 0 10px rgba(102, 51, 153, 0.5)',
+        'brand-secondary': '0 0 10px rgba(0, 204, 51, 0.5)',
+      },
+      backdropBlur: {
+        tiny: '2px',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '0.5rem',
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
