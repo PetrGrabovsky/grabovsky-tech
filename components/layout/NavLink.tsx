769d546bf539dd6a56571f840dd67e3bbfc5f823
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-11 18:51:45 +0100

    feat: Přidej podtržení pro aktivní/hoverovaný odkaz v Navigation

--- components/layout/NavLink.tsx
+++ components/layout/NavLink.tsx
@@ -7,21 +7,29 @@ import NavItem from '@/types/navItem';
 interface NavLinkProps {
   navItem: NavItem;
   isActive: boolean;
+  isHovered: boolean;
+  onHover: () => void;
+  onHoverLeave: () => void;
 }
 
-const NavLink = ({ navItem, isActive }: NavLinkProps) => {
+const NavLink = ({
+  navItem,
+  isActive,
+  isHovered,
+  onHover,
+  onHoverLeave,
+}: NavLinkProps) => {
   return (
     <Link
       href={navItem.href}
       aria-current={isActive ? 'page' : undefined}
+      onMouseEnter={onHover}
+      onMouseLeave={onHoverLeave}
       className={clsx(
         sora.className,
-        'flex items-center border-brand-secondary border-opacity-0 px-2 py-1',
-        'transition-primary justify-center space-x-2 text-text-secondary',
-        'border-b',
-        isActive
-          ? 'rounded-md bg-brand-secondary bg-opacity-40'
-          : 'bg-transparent hover:border-opacity-100'
+        'transition-primary flex items-center justify-center space-x-2 px-2',
+        'border-b border-brand-secondary py-1 text-sm text-text-secondary',
+        isHovered ? 'border-opacity-100' : 'border-opacity-0'
       )}
     >
       <navItem.Icon aria-hidden='true' className='h-4 w-auto' />
--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -1,4 +1,5 @@
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
 import navItems from '@/data/navItems';
 
@@ -6,13 +7,23 @@ import NavLink from './NavLink';
 
 const Navigation = () => {
   const pathname = usePathname() || '/';
+  const [hoveredId, setHoveredId] = useState(-1);
 
   return (
     <nav>
       <ul className='flex items-center md:flex-row md:space-x-4'>
         {navItems.map((item) => (
           <li key={item.id}>
-            <NavLink navItem={item} isActive={pathname === item.href} />
+            <NavLink
+              navItem={item}
+              isActive={pathname === item.href}
+              isHovered={
+                hoveredId === item.id ||
+                (pathname === item.href && hoveredId === -1)
+              }
+              onHover={() => setHoveredId(item.id)}
+              onHoverLeave={() => setHoveredId(-1)}
+            />
           </li>
         ))}
       </ul>
