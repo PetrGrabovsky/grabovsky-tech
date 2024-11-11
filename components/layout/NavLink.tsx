2e0a675c0ccd2a763049c69cca7294b5b4b2138e
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-11 20:56:27 +0100

    refactor: Optimalizuj komponenty NavLink a Navigation

--- components/layout/NavLink.tsx
+++ components/layout/NavLink.tsx
@@ -1,11 +1,16 @@
 import clsx from 'clsx';
 import Link from 'next/link';
+import { IconType } from 'react-icons';
 
 import { sora } from '@/styles/fonts';
-import NavItem from '@/types/navItem';
 
 interface NavLinkProps {
-  navItem: NavItem;
+  navItem: {
+    id: number;
+    href: string;
+    label: string;
+    Icon: IconType;
+  };
   isActive: boolean;
   isHovered: boolean;
   onHover: () => void;
@@ -13,29 +18,27 @@ interface NavLinkProps {
 }
 
 const NavLink = ({
-  navItem,
+  navItem: { href, label, Icon },
   isActive,
   isHovered,
   onHover,
   onHoverLeave,
-}: NavLinkProps) => {
-  return (
-    <Link
-      href={navItem.href}
-      aria-current={isActive ? 'page' : undefined}
-      onMouseEnter={onHover}
-      onMouseLeave={onHoverLeave}
-      className={clsx(
-        sora.className,
-        'transition-primary flex items-center justify-center space-x-2 px-2',
-        'border-b border-brand-secondary py-1 text-sm text-text-secondary',
-        isHovered ? 'border-opacity-100' : 'border-opacity-0'
-      )}
-    >
-      <navItem.Icon aria-hidden='true' className='h-4 w-auto' />
-      <span>{navItem.label}</span>
-    </Link>
-  );
-};
+}: NavLinkProps) => (
+  <Link
+    href={href}
+    aria-current={isActive ? 'page' : undefined}
+    onMouseEnter={onHover}
+    onMouseLeave={onHoverLeave}
+    className={clsx(
+      sora.className,
+      'transition-primary flex items-center justify-center space-x-2 px-2',
+      'border-b border-brand-secondary py-1 text-sm text-text-secondary',
+      { 'border-opacity-100': isHovered, 'border-opacity-0': !isHovered }
+    )}
+  >
+    <Icon aria-hidden='true' className='h-4 w-auto' />
+    <span>{label}</span>
+  </Link>
+);
 
 export default NavLink;
--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -7,25 +7,30 @@ import NavLink from './NavLink';
 
 const Navigation = () => {
   const pathname = usePathname() || '/';
-  const [hoveredId, setHoveredId] = useState(-1);
+  const [hoveredId, setHoveredId] = useState<number | null>(null);
+
+  const handleHover = (id: number) => setHoveredId(id);
+  const handleHoverLeave = () => setHoveredId(null);
 
   return (
     <nav>
       <ul className='flex items-center md:flex-row md:space-x-4'>
-        {navItems.map((item) => (
-          <li key={item.id}>
-            <NavLink
-              navItem={item}
-              isActive={pathname === item.href}
-              isHovered={
-                hoveredId === item.id ||
-                (pathname === item.href && hoveredId === -1)
-              }
-              onHover={() => setHoveredId(item.id)}
-              onHoverLeave={() => setHoveredId(-1)}
-            />
-          </li>
-        ))}
+        {navItems.map((item) => {
+          const isActive = pathname === item.href;
+          const isHovered = hoveredId === item.id || (!hoveredId && isActive);
+
+          return (
+            <li key={item.id}>
+              <NavLink
+                navItem={item}
+                isActive={isActive}
+                isHovered={isHovered}
+                onHover={() => handleHover(item.id)}
+                onHoverLeave={handleHoverLeave}
+              />
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
