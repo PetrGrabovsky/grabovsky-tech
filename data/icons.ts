682ecc5a9a18413c4851d3d818bb883990f12499
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-11 14:40:09 +0100

    feat: Definuj základní ikony

---
+++ data/icons.ts
@@ -0,0 +1,9 @@
+import { FaCogs } from 'react-icons/fa';
+import { HiHome, HiPhone, HiUser } from 'react-icons/hi';
+import { MdThumbUp } from 'react-icons/md';
+
+export const HomeIcon = HiHome;
+export const AboutIcon = HiUser;
+export const ProjectIcon = FaCogs;
+export const ReferenceIcon = MdThumbUp;
+export const ContactIcon = HiPhone;
