a98aa35a64b86c3ac8a5c723fbaa0d1f7bbb1912
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-11 14:28:42 +0100

    feat: Přidej stav isMobileNavOpen do LayoutProvider
    - Vytvoř funkci handleMenuButtonClick

--- providers/LayoutProvider.tsx
+++ providers/LayoutProvider.tsx
@@ -1,15 +1,20 @@
 'use client';
 
-import { createContext, ReactNode } from 'react';
+import { throttle } from 'lodash';
+import { createContext, ReactNode, useEffect, useMemo, useState } from 'react';
 
 import useIsMobileView from '@/hooks/useIsMobileView';
 
 interface LayoutContextType {
   isMobileView: boolean;
+  isMobileNavOpen: boolean;
+  handleMenuButtonClick: () => void;
 }
 
 const initialState: LayoutContextType = {
   isMobileView: true,
+  isMobileNavOpen: false,
+  handleMenuButtonClick: () => {},
 };
 
 export const LayoutContext = createContext<LayoutContextType>(initialState);
@@ -20,9 +25,32 @@ interface LayoutProviderProps {
 
 const LayoutProvider = ({ children }: LayoutProviderProps) => {
   const isMobileView = useIsMobileView(initialState.isMobileView);
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(
+    initialState.isMobileNavOpen
+  );
+
+  useEffect(() => {
+    if (!isMobileView) {
+      setIsMobileNavOpen(false);
+    }
+  }, [isMobileView]);
+
+  useEffect(() => {
+    return () => {
+      handleMenuButtonClick.cancel();
+    };
+  }, []);
+
+  const handleMenuButtonClick = useMemo(() => {
+    return throttle(() => {
+      setIsMobileNavOpen((prev) => !prev);
+    }, 300);
+  }, []);
 
   return (
-    <LayoutContext.Provider value={{ isMobileView }}>
+    <LayoutContext.Provider
+      value={{ isMobileView, isMobileNavOpen, handleMenuButtonClick }}
+    >
       {children}
     </LayoutContext.Provider>
   );
