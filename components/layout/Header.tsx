8511f02b194a3cba5e7726e7b96513b04e600850
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-11 23:10:11 +0100

    feat: Integruj Navigation pro mobilní zobrazení v Header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,17 +2,35 @@
 
 import clsx from 'clsx';
 
+import useLayout from '@/hooks/useLayout';
+
 import NavBar from './NavBar';
+import Navigation from './Navigation';
 
 const Header = () => {
+  const { isMobileView, isMobileNavOpen } = useLayout();
+
   return (
     <header
       className={clsx(
-        'layout-background fixed inset-0 h-fit border-b bg-gradient-to-b'
+        'layout-background fixed inset-0 z-50 border-b bg-gradient-to-b',
+        'transition-primary flex flex-col',
+        isMobileNavOpen ? 'h-screen' : 'h-16'
       )}
     >
-      <div className='container'>
+      <div className='container flex flex-grow flex-col'>
         <NavBar />
+        {isMobileView && (
+          <div
+            className={clsx(
+              'transition-primary flex items-center justify-center',
+              'overflow-hidden',
+              isMobileNavOpen ? 'flex-grow opacity-100' : 'h-0 opacity-0'
+            )}
+          >
+            <Navigation />
+          </div>
+        )}
       </div>
     </header>
   );
