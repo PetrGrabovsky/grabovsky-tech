8056c835eda0a8857f8a40819e9c79a19f5b8091
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-12 17:39:34 +0100

    feat: Vytvoř komponentu Footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,8 +1,60 @@
+'use client';
+
+import clsx from 'clsx';
+
+import socialLinks from '@/data/socialLinks';
+import useLayout from '@/hooks/useLayout';
+import { sora } from '@/styles/fonts';
+
 const Footer = () => {
+  const { isMobileNavOpen } = useLayout();
+
   return (
-    <footer className='layout-background flex h-12 border-t bg-gradient-to-t'>
-      <div className='container flex items-center justify-end'>
-        Všechna práva vyhrazena
+    <footer
+      aria-hidden={isMobileNavOpen}
+      className='flex h-12 flex-col justify-end'
+    >
+      <div
+        className={clsx(
+          'layout-background transition-primary w-full border-t',
+          'overflow-hidden bg-gradient-to-t',
+          isMobileNavOpen ? 'h-0' : 'h-12'
+        )}
+      >
+        <div
+          className={clsx(
+            sora.className,
+            'transition-primary container flex h-full w-full items-center',
+            'justify-between text-sm text-text-secondary',
+            isMobileNavOpen ? 'opacity-0' : 'opacity-100'
+          )}
+        >
+          <a
+            href='mailto:info@grabovsky.tech'
+            aria-label='Kontaktujte mě e-mailem na info@grabovsky.tech'
+            className='transition-primary hover:scale-110'
+          >
+            info@grabovsky.tech
+          </a>
+          <div className='flex h-full items-center space-x-3 py-2'>
+            {socialLinks.map(({ id, href, Icon, label }) => (
+              <a
+                key={id}
+                href={href}
+                aria-label={label}
+                tabIndex={isMobileNavOpen ? -1 : 0}
+                target='_blank'
+                rel='noopener noreferrer'
+                className={clsx(
+                  'transition-primary h-full w-auto p-1',
+                  'hover:scale-110'
+                )}
+              >
+                <Icon aria-hidden='true' className='h-full w-auto' />
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
     </footer>
   );
