9a158db4841ef6b232d1ab1207cab13aef268533
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-13 21:44:57 +0100

    feat: Přidej provizorní obsah do stránek

--- app/reference/page.tsx
+++ app/reference/page.tsx
@@ -16,7 +16,33 @@ const References = () => {
         consectetur, adipisicing elit. Ullam quas, voluptatibus explicabo,
         veniam laborum debitis odit recusandae rerum minus, reprehenderit sequi
         expedita accusantium natus error autem eos necessitatibus exercitationem
-        eius.
+        eius. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae
+        dolore quaerat eos eum quis, sunt dolor quibusdam architecto aliquam
+        maxime est! Incidunt officiis ipsa ducimus asperiores, eaque quaerat
+        eligendi aliquam. Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Commodi perferendis error tempora debitis fugiat distinctio soluta
+        dicta facilis explicabo nam aut, iusto id quae quisquam, ipsa dolor
+        rerum, voluptates adipisci! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Nobis sint debitis dolorum. Quidem officia fugiat
+        deleniti cum reiciendis molestias voluptatem, ducimus illo, aspernatur
+        neque distinctio itaque porro modi ipsa voluptatum! Lorem ipsum dolor
+        sit amet consectetur, adipisicing elit. Ullam quas, voluptatibus
+        explicabo, veniam laborum debitis odit recusandae rerum minus,
+        reprehenderit sequi expedita accusantium natus error autem eos
+        necessitatibus exercitationem eius. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Vitae dolore quaerat eos eum quis, sunt
+        dolor quibusdam architecto aliquam maxime est! Incidunt officiis ipsa
+        ducimus asperiores, eaque quaerat eligendi aliquam. Lorem ipsum dolor,
+        sit amet consectetur adipisicing elit. Commodi perferendis error tempora
+        debitis fugiat distinctio soluta dicta facilis explicabo nam aut, iusto
+        id quae quisquam, ipsa dolor rerum, voluptates adipisci! Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Nobis sint debitis dolorum.
+        Quidem officia fugiat deleniti cum reiciendis molestias voluptatem,
+        ducimus illo, aspernatur neque distinctio itaque porro modi ipsa
+        voluptatum! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Ullam quas, voluptatibus explicabo, veniam laborum debitis odit
+        recusandae rerum minus, reprehenderit sequi expedita accusantium natus
+        error autem eos necessitatibus exercitationem eius.
       </p>
     </section>
   );
