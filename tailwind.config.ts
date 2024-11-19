c56c5bb70a1c412400f047ec963524a194838c4a
Petr Grabovský <petr@grabovsky.tech>
2024-11-19 12:09:19 +0100

    chore: Přidej složku layout do content konfigurace v tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./shared/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
+  content: ['./shared/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './layout/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
