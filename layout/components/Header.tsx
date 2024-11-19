9d83b9ff4b70bb95d85710b4ed7a53537c4bd9fa
Petr Grabovský <petr@grabovsky.tech>
2024-11-19 14:02:47 +0100

    refactor: Uprav komponentu Header, aby příjmala children

--- layout/components/Header.tsx
+++ layout/components/Header.tsx
@@ -1,5 +1,7 @@
-const Header = () => {
-  return <header className='h-16 bg-red-500'>Header</header>;
+import { ChildrenProperties } from '@/shared/types';
+
+const Header = ({ children }: ChildrenProperties) => {
+  return <header className='h-16 bg-red-500'>{children}</header>;
 };
 
 export default Header;
