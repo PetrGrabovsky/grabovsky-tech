a83985f22996fc6c13b354be7354efd39075722e
Petr Grabovský <petr@grabovsky.tech>
2024-11-19 12:05:05 +0100

    style: Definuj základní styl elementu body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,21 @@
 import '@/shared/globals.css';
 
+import clsx from 'clsx';
+
+import { sora } from '@/shared/fonts';
 import { ChildrenProperties } from '@/shared/types';
 
 const RootLayout = ({ children }: ChildrenProperties) => {
   return (
     <html lang='cs'>
-      <body className='antialiased'>{children}</body>
+      <body
+        className={clsx(
+          sora.className,
+          'flex min-h-screen flex-col overflow-x-hidden bg-neutral-950 antialiased'
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 };
