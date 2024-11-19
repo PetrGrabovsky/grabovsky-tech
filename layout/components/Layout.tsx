838e5445719a0c8eab32bbf64cfd309930061500
Petr Grabovský <petr@grabovsky.tech>
2024-11-19 14:03:41 +0100

    feat: Vytvoř a integruj základ Navbar komponenty

--- layout/components/Layout.tsx
+++ layout/components/Layout.tsx
@@ -2,11 +2,14 @@ import { ChildrenProperties } from '@/shared/types';
 
 import Footer from './Footer';
 import Header from './Header';
+import Navbar from './Navbar';
 
 const Layout = ({ children }: ChildrenProperties) => {
   return (
     <>
-      <Header />
+      <Header>
+        <Navbar />
+      </Header>
       <main className='flex-grow'>{children}</main>
       <Footer />
     </>
---
+++ layout/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className='container flex h-full items-center justify-between bg-yellow-700 py-2'>
+      <span>Logo</span>
+      <span>Menu</span>
+    </div>
+  );
+};
+
+export default Navbar;
