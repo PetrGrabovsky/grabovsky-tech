42eba25a90495b62efd9d029cea8ec55cf420eb5
Petr Grabovský <petr@grabovsky.tech>
2024-11-19 13:45:57 +0100

    style: Definuj screens a container v tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./shared/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './layout/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '2xl': '1280px',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '1.5rem',
+          md: '2rem',
+          lg: '2.5rem',
+          xl: '3rem',
+          '2xl': '3.5rem',
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
