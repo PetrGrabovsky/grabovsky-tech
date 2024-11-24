25e84c5e7806c3da79d4e63ea1875e0207928361
Petr Grabovský <petr@grabovsky.tech>
2024-11-24 21:22:50 +0100

    style: Vytvoř animaci pro LinkedLogo

--- layout/components/LinkedLogo.tsx
+++ layout/components/LinkedLogo.tsx
@@ -1,6 +1,21 @@
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 
 import Logo from '@/shared/components/Logo';
+
+const logoVariants = {
+  initial: { scale: 1, filter: 'brightness(1)' },
+  animated: {
+    scale: [1, 0.95, 1],
+    filter: ['brightness(1)', 'brightness(0.8)', 'brightness(1)'],
+  },
+};
+
+const logoTransition = {
+  repeat: Infinity,
+  duration: 3,
+  ease: 'easeInOut',
+};
 
 const LinkedLogo = () => {
   return (
@@ -9,7 +24,15 @@ const LinkedLogo = () => {
       className='h-full w-auto'
       href='/'
     >
-      <Logo />
+      <motion.div
+        animate='animated'
+        className='h-full w-auto'
+        initial='initial'
+        transition={logoTransition}
+        variants={logoVariants}
+      >
+        <Logo />
+      </motion.div>
     </Link>
   );
 };
